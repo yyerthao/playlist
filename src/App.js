2230b955fbf237196fe124f05af9b2dd9bf46f49
Yer Thao <maivyerthao@gmail.com>
2021-09-27 23:28:34 -0500

    File set up react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { React, Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      songs: [
+        { name: 'Dreaming of You', added: '2012-09-20' },
+        { name: 'Baby Shark', added: '2016-09-09' },
+        { name: 'Wheels on the Bus', added: '2020-05-05' }
+      ]
+    }
+  }
+
+
+    render() {
+      return(
+        <div className="App">
+          <header>Playlist</header>
+            <ul>
+              {this.state.songs.map(song => 
+                <li>{song.name}</li>
+                )}
+            </ul>
+        </div>
+      ) 
+    }
 }
 
 export default App;
